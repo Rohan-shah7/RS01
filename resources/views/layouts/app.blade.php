d08c33ae0ee8b44aa2fa6889456bf1b003f766c4
Rohan-shah7 <rohanshah2090@gmail.com>
2025-08-08 18:32:12 +0545

    added jquery ,momentjs and sweet alert

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -32,5 +32,11 @@
                @yield('content')
             </main>
         </div>
+
+         {{-- jQuery --}}
+    <script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.30.1/moment.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    @stack('jquery')
     </body>
 </html>
